0611700c121eb4873c5a52a7712d42a65a89226a
ProgrammerChris6002 <ProgrammerChris6002@gmail.com>
2026-01-16 07:37:44 +0100

    making inventory responsive

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,13 +1,13 @@
 import { stackServerApp } from "@/stack/server";
-// import { redirect } from "next/navigation";
-import { NextResponse } from "next/server";
+import { redirect } from "next/navigation";
+// import { NextResponse } from "next/server";
 
 export async function getCurrentUser() {
   const user = await stackServerApp.getUser()
 
   if (!user) {
-    // redirect("/handler/sign-in");
-    return NextResponse.next();
+    redirect("/handler/sign-in");
+    // return NextResponse.next();
   };
 
   return (user);
